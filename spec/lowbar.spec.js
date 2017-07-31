0b6ecabeebc9f49c647143121aca873d94db437e
jonsey247 <jacobfilmjones@hotmail.com>
2017-07-31 10:31:56 +0100

    #identity further tests implimented and all passing

--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -5,16 +5,42 @@ var expect = require('chai').expect;
 
 var _ = require(path.join(__dirname, '..', './lowbar.js'));
 
-describe('_', function() {
+describe('_', function () {
     'use strict';
 
-    it('is an object', function() {
+    it('is an object', function () {
         expect(_).to.be.an('object');
     });
 
-    describe('#identity', function() {
-        it('is a function', function() {
+    describe('#identity', function () {
+        it('is a function', function () {
             expect(_.identity).to.be.a('function');
         });
+        it('should return the same data type', function () {
+            var result = _.identity([1, 2, 3]);
+            expect(Array.isArray(result)).to.equal(true);
+            var nums = _.identity(4567);
+            expect(Number.isInteger(nums)).to.equal(true);
+            var str = _.identity('hello world!');
+            expect(typeof str).to.be.a('string');
+            var undef = _.identity(undefined);
+            expect(undef).to.be.a('undefined');
+            var boolean = _.identity(true);
+            expect(boolean).to.be.a('boolean');
+            var nul = _.identity(null);
+            expect(nul).to.be.a('null');
+        });
+
+        it('should return the given value', function () {
+            var result = _.identity(5);
+            expect(result).to.equal(5);
+        });
     });
-});
+
+//     //first
+//     describe('#first', function() {
+//         it('is a function', function() {
+//             expect(_.first).to.be.a('function');
+//         });
+//     });
+ });
