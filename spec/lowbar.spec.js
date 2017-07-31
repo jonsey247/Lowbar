9362f155fd16d0c20f70ec3b85a484a4877b0105
jonsey247 <jacobfilmjones@hotmail.com>
2017-07-31 10:20:31 +0100

    initial commit

---
+++ spec/lowbar.spec.js
@@ -0,0 +1,6 @@
+// /* global describe, it */
+// var path = require('path');
+// var expect = require('chai').expect;
+// var sinon = require('sinon');
+
+// var _ = require(path.join(__dirname, '..', './lowbar.js'));
