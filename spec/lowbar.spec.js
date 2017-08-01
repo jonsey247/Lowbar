ef6819abcf6df4b5a66d3269c5096152d95910ed
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-01 14:48:29 +0100

    #every, all tests passing

--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -245,5 +245,13 @@ describe('_', function () {
     it('is a function', function() {
       expect(_.every).to.be.a('function');
     });
+
+    it('returns false if not all list values pass the predicate test', function() {
+      expect(_.every([2, 4, 5], function(num) { return num % 2 == 0; })).to.equal(false);
+    });
+
+    it('returns true if all list values pass the predicate test', function() {
+      expect(_.every([2, 4], function(num) { return num % 2 == 0; })).to.equal(true);
+    });
   });
 });
