0e4732f1e3698e7d85bc943e40b349debf527f84
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-01 11:06:52 +0100

    #reject, all tests passing

--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -158,6 +158,13 @@ describe('_', function () {
   describe('#reject', function() {
     it('is a function', function() {
       expect(_.reject).to.be.a('function');
+    });
+
+    it('should return a list filtered by the predicate', function() {
+      var result = _.reject([1,2,3,4,5,6], function(num) {return num % 2 === 0;});
+      expect(result).to.eql([1,3,5]);
+      var result2 = _.reject({a:1, b:2, c:3, d:4, e:5, f:6}, function(num) {return num % 2 === 0;});
+      expect(result2).to.eql([1,3,5]);
     });   
   });
 });
